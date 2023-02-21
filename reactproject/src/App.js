427e31d3534c2210ca368d97300db3044332ce09
Lee Yejin <lightup.jin@gmail.com>
2023-02-21 18:09:28 +0900

    ⚡️Reference data type

--- reactproject/src/App.js
+++ reactproject/src/App.js
@@ -1,18 +1,22 @@
 /* eslint-disable */
 
-import logo from './logo.svg';
-import './App.css';
-import {useState} from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  let list = 'DW아카데미 503호';
+  let list = "DW아카데미 503호";
   // 바닐라JS 코드 (변수에 있는 데이터 html에 넣기)
   // JSX 중괄호 문법을 사용하면 더 쉽고 간단한게 할 수 있다 (id, className, src, href등 사용 가능!)
   // document.querySelector('h4').innerHTML = list;
 
   // 리액트에서 변수처럼 자료를 보관할 수 있는 satate 문법
   // 한 곳에다가 여러가지 자료를 저장하고 싶을 때는 대괄호를 열어서 Array자료 형식으로 사용하면 편리하다
-  let [title, setTitle] = useState(['DW아카데미 501호', 'DW아카데미 503호', 'DW아카데미 201호']);
+  let [title, setTitle] = useState([
+    "DW아카데미 501호",
+    "DW아카데미 503호",
+    "DW아카데미 201호",
+  ]);
   // let [title2, setTitle2] = useState('DW아카데미 503호');
   // let [title3, setTitle3] = useState('DW아카데미 201호');
   // 왼쪽 : 실제 state값 / 오른쪽 : 변경될 state값
@@ -21,17 +25,36 @@ function App() {
   return (
     <div className="App">
       <div className="black-nav">
-        <h3 style={{color : 'red'}}>BLOG</h3>
+        <h3 style={{ color: "red" }}>BLOG</h3>
       </div>
       <div className="list">
-        <h4>{title[0]} <span onClick={()=>{setLike(like + 1)}}>👍</span> {like}</h4>
+        <h4>
+          {title[0]}{" "}
+          <span
+            onClick={() => {
+              setLike(like + 1);
+            }}
+          >
+            👍
+          </span>{" "}
+          {like}
+        </h4>
         <p>안녕하세요. 저는 이예진 입니다.</p>
-        <button onClick={()=>{
-          let titleCopy = [...title];
-          titleCopy[0] = '리액트 너무 재밌어요!';
-          console.log(title == titleCopy)
-          setTitle(titleCopy);
-        }}>Click</button>
+        <button
+          onClick={() => {
+            let titleCopy = [...title];
+            titleCopy[0] = "리액트 너무 재밌어요!";
+            console.log(title == titleCopy);
+            setTitle(titleCopy);
+
+            // let arr = [1, 2, 3];
+            // arr[0] = 3;
+            // let arrCopy = arr;
+            // console.log(arr == arrCopy);
+          }}
+        >
+          Click
+        </button>
       </div>
       <div className="list">
         <h4>{title[1]}</h4>
@@ -41,11 +64,15 @@ function App() {
         <h4>{title[2]}</h4>
         <p>안녕하세요. 저는 이예진 입니다.</p>
       </div>
-      <button onClick={()=>{
-        let copy = [...title];
-        copy.sort();
-        setTitle(copy);
-      }}>가나다순 정렬</button>
+      <button
+        onClick={() => {
+          let copy = [...title];
+          copy.sort();
+          setTitle(copy);
+        }}
+      >
+        가나다순 정렬
+      </button>
     </div>
   );
 }
