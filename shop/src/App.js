07dd48423b8b7f41285dc49112338095f7e74fba
Lee Yejin <lightup.jin@gmail.com>
2023-02-28 16:15:40 +0900

    ⚡️ Added React Bootstrap Card

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,9 +1,26 @@
-import logo from './logo.svg';
+/* eslint-disable */
+
+import logo from "./logo.svg";
 // or less ideally
-import { Button, Navbar, Container, Nav } from 'react-bootstrap';
-import './App.css';
+import {
+  Button,
+  Navbar,
+  Container,
+  Nav,
+  Card,
+  Row,
+  Col,
+} from "react-bootstrap";
+import "./App.css";
+import bgImage from "./images/쿼카.png";
+import data from "./data";
+import { useState } from "react";
+
 
 function App() {
+  let [items, setItems] = useState(data);
+// console.log(items[0].price)
+
   return (
     <div className="App">
       <Navbar bg="dark" variant="dark">
@@ -16,6 +33,41 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
+
+      {/* 코드 사이에 변수 넣는 방법 1.홑 따옴표 2. 백틱 */}
+      <div
+        className="main-bg"
+        style={{ backgroundImage: `url(${bgImage})` }}
+      ></div>
+
+      <Row className="card-list">
+        <ItemList/>
+        <ItemList/>
+        <ItemList/>
+
+        <Col>
+          <Card className="text-center card-item">
+            <Card.Header>{items[1].title}</Card.Header>
+            <Card.Body>
+              <img src={bgImage} alt="" width="50%" />
+              <Card.Title>{items[1].content}</Card.Title>
+              <Card.Text>{items[1].price}</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        <Col>
+          <Card className="text-center card-item">
+            <Card.Header>{items[2].title}</Card.Header>
+            <Card.Body>
+              <img src={bgImage} alt="" width="50%" />
+              <Card.Title>{items[2].content}</Card.Title>
+              <Card.Text>{items[2].price}</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+
       <Button variant="primary" size="lg">
         Block level button
       </Button>
@@ -23,5 +75,20 @@ function App() {
   );
 }
 
-export default App;
 
+function ItemList(props) {
+  return(
+    <Col>
+      <Card className="text-center card-item">
+        <Card.Header>{props.title}</Card.Header>
+        <Card.Body>
+          <img src={bgImage} alt="" width="50%" />
+          <Card.Title>dfghsdg</Card.Title>
+          <Card.Text>sdhdsh</Card.Text>
+        </Card.Body>
+      </Card>
+    </Col>
+  )
+}
+
+export default App;
