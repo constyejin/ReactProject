483ea4cef6dbcef87715336035697e3aa62474d5
yejin <ingkejin@gmail.com>
2023-03-06 07:37:19 +0900

    ✅ map함수로 html반복, component 활용

--- reactproject/src/App.js
+++ reactproject/src/App.js
@@ -16,11 +16,31 @@ function App() {
     console.log("num", num, "state", count)
   }
   
+  let name = ['예진1', '예진2', '예진3', '예진4']
+
   return (
     <div className="App">
+      <div className='box'>
+        <h1>box1</h1>
+        <h2>yejin</h2>
+      </div>
+      
+      {/* {
+        name.map(function(a, i){
+          return (
+            <div className='box' key={i}>
+              <h1>box1</h1>
+              <h2>{name[i]}</h2>
+              <button>button</button>
+            </div>
+          )
+        })
+      } */}
+
       <Box name="코알라" num="1"/>
-      <Box name="침팬치" num="2"/>
+      <Box name="쿼카" num="2"/>
       <Box name="수달" num="3"/>
+      <Box name="해달" num="3"/>
 
       <div>{count}</div>
       <button onClick={increase}>+1</button>
