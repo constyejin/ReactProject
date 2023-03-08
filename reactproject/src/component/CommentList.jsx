d369173ae1964bfa84fa67516b786ab5b1d215fd
Lee Yejin <lightup.jin@gmail.com>
2023-03-08 15:41:48 +0900

    ⚡️onchange에 대한 설명 추가

--- reactproject/src/component/CommentList.jsx
+++ reactproject/src/component/CommentList.jsx
@@ -24,32 +24,32 @@ function CommentList(props) {
     const [name, setName] = useState('');
     const [content, setContent] = useState('');
 
-    // const addComment = () => {
-    //   // const add = {name: "히히히", comment: "아아아앙악!! 리액트!!"}
-    //   // setCommentList([add, ...commentList])
-
-    //   // comments 배열 name(key)에는 input value(name)
-    //   const addList = {name: name, comment: content}
-    //   // Spread 
-    //   setCommentList([addList, ...commentList]);
-    //   // state 변경 함수에 빈 문자열을 넣어서 input에 작성됐던 내용을 초기화 시킨다.
-    //   // 이름, 댓글을 추가한 뒤에, 빈 문자열이 들어가서 초기화되고, 다시 다른 값을 입력 받는다.
-    //   setName('');
-    //   setContent('');
-    // }
-
     const addComment = () => {
-          name === '' || content === ''
-          ? alert('이름과 댓글을 입력하세요.')
-          : (
-            (() => {
-              const addList = { name: name, comment: content };
-              setCommentList([addList, ...commentList]);
-              setName('');
-              setContent('');
-            })()
-          );
-      };
+      // const add = {name: "히히히", comment: "아아아앙악!! 리액트!!"}
+      // setCommentList([add, ...commentList])
+
+      // comments 배열 name(key)에는 input value(name)
+      const addList = {name: name, comment: content}
+      // Spread 
+      setCommentList([addList, ...commentList]);
+      // state 변경 함수에 빈 문자열을 넣어서 input에 작성됐던 내용을 초기화 시킨다.
+      // 이름, 댓글을 추가한 뒤에, 빈 문자열이 들어가서 초기화되고, 다시 다른 값을 입력 받는다.
+      setName('');
+      setContent('');
+    }
+
+    // const addComment = () => {
+    //       name === '' || content === ''
+    //       ? alert('이름과 댓글을 입력하세요.')
+    //       : (
+    //         (() => {
+    //           const addList = { name: name, comment: content };
+    //           setCommentList([addList, ...commentList]);
+    //           setName('');
+    //           setContent('');
+    //         })()
+    //       );
+    //   };
       
 
 
@@ -75,8 +75,11 @@ function CommentList(props) {
         })
       }
 
-
-      이름 : <input value={name} onChange={(e) => setName(e.target.value)}></input>
+      {/* onChange 이벤트는 input 요소에서 값이 변경될 때마다 발생한다. 이때 발생한 이벤트를 매개변수 e로 받아온다. */}
+      {/* e.target은 이벤트가 발생한 요소(input 요소)
+          e.target.value는 input 요소의 값(value 속성)
+          setName과 setContent 함수는 각각 input의 value를 state 변수인 name과 content에 업데이트 해준다. */}
+      이름 : <input value={name} onChange={(e) => setName(e.target.value)}/>
       댓글 : <input value={content} onChange={(e) => setContent(e.target.value)} />
 
       {/* <input onChange={(e)=>{setContent(e.target.value)}} /> */}
