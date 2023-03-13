75c78ad255dc01c66d3b47ff9b361fb5d27f97b1
yejin <ingkejin@gmail.com>
2023-03-14 00:15:08 +0900

    ⚡️ styled-components 장.단점

--- shop/src/pages/Detail.js
+++ shop/src/pages/Detail.js
@@ -1,4 +1,25 @@
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+
+let Btn = styled.button`
+  // 외부 라이브러리 사용법 (props 사용)
+  background-color: ${props => props.bg};
+  color : white;
+  padding : 10px;
+  border : none;
+  border-radius : 5px;
+`
+
+let Box = styled.div`
+  width : 100px;
+  height: 200px;
+  background-color : #000;
+`
+
+let NewBtn = styled(Btn)`
+  margin : 10px;
+  border: 2px solid ${props => props.bg == 'black' ? '#fff' : 'red'}
+`
 
 function Detail(props) {
   let {id} = useParams();
@@ -9,6 +30,12 @@ function Detail(props) {
 
   return (
     <div className="container">
+      <Box>
+       <Btn bg="blue">Button</Btn>
+       <Btn bg="red">Button</Btn>
+       <Btn bg="green">Button</Btn>
+       <NewBtn bg="black">BTN</NewBtn>
+      </Box>
       <div className="row">
         <div className="col-md-6">
           <img src={"https://codingapple1.github.io/shop/shoes"+ (props.shoes[id].id+1) +".jpg"} width="100%" />
