3436f5447a91dfe2d28fef7fe28530428cb47486
yejin <ingkejin@gmail.com>
2023-03-13 23:38:58 +0900

    ⚡️ useParams를 이용한 링크 이동

--- shop/src/App.js
+++ shop/src/App.js
@@ -15,10 +15,14 @@ import { useState } from "react";
 import data from "./data";
 import bgImage1 from "./images/쿼카.png";
 import bgImage2 from './images/bg.png'
-import {Routes, Route, Link} from 'react-router-dom';
+import {Routes, Route, Link, useNavigate, Outlet} from 'react-router-dom';
+import Detail from './pages/Detail';
 
 function App() {
   let [shoes] = useState(data);
+  // useNavigate Hook : 페이지 이동을 도와주는 함수
+  // Hook : 유용한 것들이 모아져 있는 함수
+  let navigate = useNavigate();
 
   return (
     <div className="App">
@@ -26,8 +30,9 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">Yejin Shop</Navbar.Brand>
           <Nav className="me-auto">
-            <Link to="/" className='nav-link'>Home</Link>
-            <Link to="/detail" className='nav-link'>Detail</Link>
+            <Nav.Link onClick={()=>{navigate
+            ('/')}} className='nav-link'>Home</Nav.Link>
+            <Nav.Link onClick={()=>{navigate('./detail')}} className='nav-link'>Detail</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
@@ -45,7 +50,7 @@ function App() {
               {
                 shoes.map((a, i)=>{
                   return (
-                    <Card shoes={shoes[i]} i={i}/>
+                    <Card shoes={shoes[i]} i={i} key={i}/>
                   )
                 })
               }
@@ -65,7 +70,39 @@ function App() {
             </Row>
           </div>
         </div>}/>
-        <Route path='/detail' element={<div>상세 페이지</div>}/>
+
+        {/* <Route path='/detail/0' element={<Detail shoes={shoes}/>}/>
+        <Route path='/detail/1' element={<Detail shoes={shoes}/>}/>
+        <Route path='/detail/2' element={<Detail shoes={shoes}/>}/> */}
+
+        <Route path='/detail/:id' element={<Detail shoes={shoes} id={shoes.id}/>}/>
+
+        {/* <Route path='/detail' element={
+          <div>
+            {
+              shoes.map((a, i)=>{
+                return (
+                  <Detail shoes={shoes[i]} i={i} key={i}/>
+                )
+              })
+            }
+          </div>
+        }/> */}
+
+        <Route path='/about' element={<About/>}>
+          <Route path='member' element={<div>멤버</div>}/>
+          <Route path='location' element={<div>위치정보다!</div>}/>
+        </Route>
+
+
+        <Route path='/about/member' element={<About/>}/>
+        <Route path='/about/location' element={<About/>}/>
+        <Route path='*' element={<h1>404 ERROR</h1>}/>
+
+        <Route path='/event' element={<Event/>}>
+          <Route path='/event/one' element={<p>첫 주문시 양배추즙 서비스</p>}/>
+          <Route path='/event/two' element={<p>생일기념 쿠폰받기</p>}/>
+        </Route>
       </Routes>
     </div>
   );
@@ -77,8 +114,26 @@ function Card(props){
       <img src={"https://codingapple1.github.io/shop/shoes" + (props.i+1) + ".jpg"} width="80%" />
       <h4>{props.shoes.title}</h4>
       <p>{props.shoes.content}</p>
-      <p>{props.shoes.price}</p>
+      <p>{props.shoes.price}won</p>
     </Col>
+  )
+}
+
+function About() {
+  return (
+    <div>
+      <h3>회사 정보 어쩌구~ 저쩌구~</h3>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
+function Event() {
+  return(
+    <div>
+      <h1>오늘의 이벤트</h1>
+      <Outlet></Outlet>
+    </div>
   )
 }
 
