b542c515cbde479852b28343e2147205d3b7b121
yejin <ingkejin@gmail.com>
2023-03-22 09:00:06 +0900

    ✨ Created a new Component

---
+++ shoppingmall/src/component/ProductCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+      <img src="https://static.nike.com/a/images/t_PDP_864_v1,f_auto,q_auto:eco/ff277d04-1977-4caa-be23-3cea973d2575/%EB%8D%A9%ED%81%AC-%EB%A1%9C%EC%9A%B0-%EC%97%AC%EC%84%B1-%EC%8B%A0%EB%B0%9C-ZuZyA5Sj.png" alt="" />
+      <div>md선택상품</div>
+      <div>제품명</div>
+      <div>가격</div>
+      <div>신제품</div>
+    </div>
+  )
+}
+
+export default ProductCard
