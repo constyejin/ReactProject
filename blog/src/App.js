ccf1fee3d4da88ef40796e936d35a36db8fb680f
constyejin <lightup.jin@gmail.com>
2023-11-22 18:02:34 +0900

    ✨ JSX Basic Syntax

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,22 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let post = '배고픈데';
+
+  // JSX 문법
+  // 1. class 넣을 때 => className
+  // 2. 데이터 바인딩 => {변수명}
+  // 3. style => style={{이름 : '값'}}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='nav'>
+        <h4 style={{fontSize : '24px'}}>BLOG</h4>
+      </div>
+      <h4 style={{color : 'red'}}>{post}</h4>
     </div>
   );
 }
