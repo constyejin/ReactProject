83cae724e413d166a663422821034724260afb37
constyejin <lightup.jin@gmail.com>
2023-11-24 18:59:54 +0900

    ✨ Add shoes imge key value

---
+++ part2/src/data.js
@@ -0,0 +1,29 @@
+let data = [
+  {
+    id : 0,
+    img : 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/88c9e86c-58d5-4cfc-b1ee-e18213a5ef2e/%EC%BD%94%EB%A5%B4%ED%85%8C%EC%A6%88-%EC%97%AC%EC%84%B1-%EC%8B%A0%EB%B0%9C-ak3bBZzW.png',
+    title : "나이키 코르테즈",
+    content : "여성 신발",
+    price : 139000
+  },
+
+  {
+    id : 1,
+    img : 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/bf66bd79-b7de-4263-8e93-87ee9ed6489b/%EB%8D%A9%ED%81%AC-%EB%A1%9C%EC%9A%B0-%ED%94%84%EB%A6%AC%EB%AF%B8%EC%97%84-%EC%97%AC%EC%84%B1-%EC%8B%A0%EB%B0%9C-QPzSIPpT.png',
+    title : "나이키 덩크 로우 프리미엄",
+    content : "여성신발",
+    price : 159000
+  },
+
+  {
+    id : 2,
+    img: 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/e777c881-5b62-4250-92a6-362967f54cca/%EC%97%90%EC%96%B4-%ED%8F%AC%EC%8A%A4-1-07-%EC%97%AC%EC%84%B1-%EC%8B%A0%EB%B0%9C-nCaVAw47.png',
+    title : "나이키 에어 포스 1'07",
+    content : "여성 신발",
+    price : 139000
+  }
+] 
+
+export default data;
+// export 여러개 하려면
+// export {변수1, 변수2}
