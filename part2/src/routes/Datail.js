42a77cd2124457e303ad1accaacd9364474af833
constyejin <lightup.jin@gmail.com>
2023-11-26 17:48:17 +0900

    ♻️ data 고유 id 값으로 상세페이지 주소 변경

--- part2/src/routes/Datail.js
+++ part2/src/routes/Datail.js
@@ -3,17 +3,24 @@ import { useParams } from 'react-router-dom';
 
 function Detail(props){
   let {id} = useParams();
+  // .find(), .filter() 사용
+  // array 자료 안에서 원하는 항목 찾기
+  // find() 콜백함수에 파라미터 넣으면 array에 있던 자료를 뜻한다. (x)
+  let findItem = props.shoes.find(function(x){
+    return x.id == id;
+  })
 
+  console.log(findItem)
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-6">
-          <img src={props.shoes[id].img} width="100%" />
+          <img src={findItem.img} width="100%" />
         </div>
         <div className="col-md-6">
-          <h4 className="pt-5">{props.shoes[id].title}</h4>
-          <p>{props.shoes[id].content}</p>
-          <p>{props.shoes[id].price}</p>
+          <h4 className="pt-5">{findItem.title}</h4>
+          <p>{findItem.content}</p>
+          <p>{findItem.price}</p>
           <button className="btn btn-danger">주문하기</button> 
         </div>
       </div>
