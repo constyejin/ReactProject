fac624f50fda909e5b57434e91c3ecbb1dd57c0f
constyejin <lightup.jin@gmail.com>
2023-11-26 16:17:30 +0900

    ♻️ useParams( ) 값으로 데이터 값 수정

--- part2/src/routes/Datail.js
+++ part2/src/routes/Datail.js
@@ -1,14 +1,19 @@
-function Detail(){
+import { useParams } from 'react-router-dom';
+
+
+function Detail(props){
+  let {id} = useParams();
+
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-6">
-          <img src="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/88c9e86c-58d5-4cfc-b1ee-e18213a5ef2e/%EC%BD%94%EB%A5%B4%ED%85%8C%EC%A6%88-%EC%97%AC%EC%84%B1-%EC%8B%A0%EB%B0%9C-ak3bBZzW.png" width="100%" />
+          <img src={props.shoes[id].img} width="100%" />
         </div>
         <div className="col-md-6">
-          <h4 className="pt-5">상품명</h4>
-          <p>상품설명</p>
-          <p>139000</p>
+          <h4 className="pt-5">{props.shoes[id].title}</h4>
+          <p>{props.shoes[id].content}</p>
+          <p>{props.shoes[id].price}</p>
           <button className="btn btn-danger">주문하기</button> 
         </div>
       </div>
