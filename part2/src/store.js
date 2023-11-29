b4a381cc35bcb7dac10a6d0675dc99dfe3dbd3a4
constyejin <lightup.jin@gmail.com>
2023-11-29 18:19:53 +0900

    ✨ Change the state of an object called user

--- part2/src/store.js
+++ part2/src/store.js
@@ -4,17 +4,24 @@ import { configureStore, createSlice } from '@reduxjs/toolkit'
 // state 하나를 slice라고 부른다.
 let user = createSlice({
   name : 'user',
-  initialState : 'Lee',
+  initialState : {name : 'Lee', age : 30},
 
   // Redux state 값 변경
   reducers : {
     changeName(state){
-      return 'Yejin' + state
+      // return {name : 'Yejin Lee', age : 30}
+      // array / object의 경우 직접 수정해도 state 변경됨 (immer.js 도움 -> 알아서 state 복사본 리턴)
+      state.name = 'Yejin Lee'
+    }
+  },
+  reducers : {
+    plusAge(state){
+      state.age = state.age + 1
     }
   }
 })
 
-export let {changeName} = user.actions
+export let {changeName, plusAge} = user.actions
 
 let stock = createSlice({
   name : 'stock',
@@ -26,15 +33,8 @@ let item = createSlice({
   initialState : [
     {id : 0, name : '나이키 코르테즈', count : 2},
     {id : 1, name : '나이키 덩크 로우 프리미엄', count : 10}
-  ],
-  reducers : {
-    plusCount(state){
-      return state + 1
-    }
-  } 
+  ]
 })
-
-export let {plusCount} = item.actions
 
 export default configureStore({
   reducer: { 
