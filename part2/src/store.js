c78feb8d2ad587a54176b75c1763a3fba76e714e
constyejin <lightup.jin@gmail.com>
2023-11-29 16:41:51 +0900

    ✨ Creating state with Redux

--- part2/src/store.js
+++ part2/src/store.js
@@ -1,7 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, createSlice } from '@reduxjs/toolkit'
+
+// useState() 역할
+// state 하나를 slice라고 부른다.
+let user = createSlice({
+  name : 'user',
+  initialState : 'Lee'
+})
 
 export default configureStore({
   reducer: { 
-    
+    user : user.reducer
   }
 }) 
