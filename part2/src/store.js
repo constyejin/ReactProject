b37b0fe335b386445b92b339360d37a35c8a57f0
constyejin <lightup.jin@gmail.com>
2023-11-29 18:43:02 +0900

    🔥 Separate user state code

--- part2/src/store.js
+++ part2/src/store.js
@@ -1,29 +1,8 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit'
+import user from './store/userSlice'
 
 // useState() 역할
 // state 하나를 slice라고 부른다.
-let user = createSlice({
-  name : 'user',
-  initialState : {name : 'Lee', age : 30},
-
-  // Redux state 값 변경
-  reducers : {
-    changeName(state){
-      // return {name : 'Yejin Lee', age : 30}
-      // array / object의 경우 직접 수정해도 state 변경됨 (immer.js 도움 -> 알아서 state 복사본 리턴)
-      state.name = 'Yejin Lee'
-    },
-
-    plusAge(state, action){
-      // state 변경함수에 파라미터 뚫는 법
-      // action : state 변경 함수
-      state.age += action.payload
-    },
-  }
-})
-
-
-export let {changeName, plusAge} = user.actions
 
 let stock = createSlice({
   name : 'stock',
