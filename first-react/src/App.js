4d15906a8f3cf3d4cc5905e3226e345044a6f409
constyejin <lightup.jin@gmail.com>
2024-01-14 23:13:32 +0900

    📝 재랜더링시 변수값

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -7,6 +7,8 @@ function App() {
   let [counter2, setCounter2] = useState(0);
 
   const increase = () => {
+    // state를 변경하면 component가 재랜더링 되면서 변수 값을 초기화 시킨다.
+    // 유저 버튼 클릭 -> counter += 1 -> setState함수 호출 -> console.log 실행 -> 변수값 1, state는 전 값 -> 함수 끝 -> App 재랜더링 -> let counter = 0으로 초기화 -> update된 state값 출력
     counter += 1;
     // state 변경함수는 비동기적으로 작동한다. (state 함수를 이용하면 그 전 값이 나온다.)
     // 함수가 끝날 때 까지 기다렸다가 state 바꾸는 함수들을 모아서 한번에 실행한다.
