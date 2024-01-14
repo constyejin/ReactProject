5ac7556761868ab3a7b523e7120f22bef4d241be
constyejin <lightup.jin@gmail.com>
2024-01-14 20:52:30 +0900

    ✨ Object Shorthand Assignment

---
+++ syntax.js
@@ -0,0 +1,12 @@
+// 리액트에 필요한 Javascript 최신 문법(ES6)
+
+// 01. Object Shorthand Assignment
+let name = 'john';
+let age = 15;
+
+// key, value 값이 일치할 때는 생략 가능
+let person = {
+  name,
+  age 
+}
+console.log(person)
