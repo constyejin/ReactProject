4c2f275c6aa808d8661f3bd14950d8274e8be6f2
constyejin <lightup.jin@gmail.com>
2024-01-16 00:18:33 +0900

    ✨ Create Box component

--- first-react/rock_paper_scissors/src/App.js
+++ first-react/rock_paper_scissors/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Box from './component/Box';
 
 // 1. 박스 2개 (타이틀, 사진, 결과)
 // 2. 가위, 바위, 보 버튼이 있다.
@@ -6,11 +7,11 @@ import './App.css';
 // 4. 컴퓨터는 랜던한 값이 선택되게 한다.
 // 5. 3,4의 결과를 가지고 승패를 따진다.
 // 6. 승패 결과에 따라 border-color를 변경한다. 
-// (win - green, lost - red, tie - black)
+// (win - green, lose - red, tie - black)
 function App() {
   return (
     <div>
-
+      <Box/>
     </div>
   );
 }
---
+++ first-react/rock_paper_scissors/src/component/Box.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Box = () => {
+  return (
+    <div className='box'>
+      <h1>You</h1>
+      <img src="https://image.yes24.com/momo/TopCate3686/MidCate6/368554599.jpg" alt="scissors" />
+      <h3>WIN</h3>
+    </div>
+  )
+}
+
+export default Box
