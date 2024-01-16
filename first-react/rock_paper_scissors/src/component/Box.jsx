317aa8353424493a72f7cb73ff48501557c093a0
constyejin <lightup.jin@gmail.com>
2024-01-16 22:18:12 +0900

    ✨ props.item.img

--- first-react/rock_paper_scissors/src/component/Box.jsx
+++ first-react/rock_paper_scissors/src/component/Box.jsx
@@ -4,7 +4,7 @@ const Box = (props) => {
   return (
     <div className='box'>
       <h1>{props.title}</h1>
-      <img src="https://image.yes24.com/momo/TopCate3686/MidCate6/368554599.jpg" alt="scissors" />
+      <img src={props.item.img} alt="scissors" />
       <h3>WIN</h3>
     </div>
   )
