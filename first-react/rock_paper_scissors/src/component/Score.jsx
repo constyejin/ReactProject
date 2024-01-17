bcbc49e41cbfdd0e8676848ae2946c2c6dfb9c99
constyejin <lightup.jin@gmail.com>
2024-01-17 18:13:30 +0900

    📝 Add Score

--- first-react/rock_paper_scissors/src/component/Score.jsx
+++ first-react/rock_paper_scissors/src/component/Score.jsx
@@ -6,11 +6,12 @@ const Score = (props) => {
   }
   
   return (
-    <div className='score-box'>
+    <div className="score-box">
       <div className="score">
-        <p>{props.score.userScore}</p>
-        <p> : </p>
-        <p>{props.score.comScore}</p>
+        <h3>Score</h3>
+        <h3>{props.score.userScore}</h3>
+        <h3 className="colon"> : </h3>
+        <h3>{props.score.comScore}</h3>
       </div>
       <button onClick={resetScore} className='reset-btn'>Reset</button>
     </div>
